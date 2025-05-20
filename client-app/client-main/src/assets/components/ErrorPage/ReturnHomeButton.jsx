da90f1d5c8ea57c2d40ed06c68c1ad4520f5aa2d
Maria Fernanda <mafenandaup@gmail.com>
2025-05-20 16:15:58 -0300

    build: import solid icons

--- client-app/client-main/src/assets/components/ErrorPage/ReturnHomeButton.jsx
+++ client-app/client-main/src/assets/components/ErrorPage/ReturnHomeButton.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
 
 const ReturnHomeButton = () => {
     return (
