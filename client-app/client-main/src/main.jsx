b763320e9832803c8e41fa508e6995edd35a58a5
Maria Fernanda <mafenandaup@gmail.com>
2025-05-20 14:57:56 -0300

    refactor: import test pages

--- client-app/client-main/src/main.jsx
+++ client-app/client-main/src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { createBrowserRouter, RouterProvider, } from 'react-router-dom'
+
+import Home from './assets/routes/Home.jsx'
+import OtherPage from './assets/routes/OtherPage.jsx'
+
+const router = createBrowserRouter([])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
