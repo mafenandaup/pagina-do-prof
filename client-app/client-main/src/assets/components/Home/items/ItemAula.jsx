e2177d5417156b166956bc2398f7db701b9465cb
Maria Fernanda <mafenandaup@gmail.com>
2025-06-02 17:10:58 -0300

    refactor: edit itemaula

--- client-app/client-main/src/assets/components/Home/items/ItemAula.jsx
+++ client-app/client-main/src/assets/components/Home/items/ItemAula.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faBook } from '@fortawesome/free-solid-svg-icons'
 import "./items.css";
 
 const ItemAula = () => {
     return (
         <>
             <div className="item-container">
-                <h1></h1>
-                <div className="icon"><FontAwesomeIcon icon={faUser} /></div>
-</div>
+                <div className="icon"><FontAwesomeIcon icon={faBook} /></div>
+            </div>
         </>
     )
 }
