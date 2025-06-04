604fe8dfffb22dd6920f5d2169b4f2adc6ec4022
Maria Fernanda <mafenandaup@gmail.com>
2025-06-04 16:10:59 -0300

    refactor: FORMS

---
+++ client-app/client-main/src/assets/routes/CreateItem/FormAluno.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CreateItem.css'
+
+const FormAluno = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FormAluno
---
+++ client-app/client-main/src/assets/routes/CreateItem/FormAula.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CreateItem.css'
+
+const FormAula = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FormAula
